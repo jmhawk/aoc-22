cc80ee780d3af20584fbdb2ddc343ffe033b4fb4
Jacob Hawk <jhawk@splashfinancial.com>
2021-12-02 11:46:18 -0500

    day 2 done

--- src/2.clj
+++ src/2.clj
@@ -10,17 +10,44 @@
 (take 10 commands)
 
 (defn part-one
-  [commands]
-  (let [{:keys [forward up down]} (group-by first commands)
+  [command-coll]
+  (let [{:keys [forward up down]} (group-by first command-coll)
         forward-sum (reduce (fn [counter entry] (+ counter (last entry))) 0 forward)
         up-sum (reduce (fn [counter entry] (+ counter (last entry))) 0 up)
         down-sum (reduce (fn [counter entry] (+ counter (last entry))) 0 down)]
     (* forward-sum (- down-sum up-sum))))
 
-(part-one commands)
+;;;;;;;; Part 2 ;;;;;;;;;
 
+(def starting-state {:depth 0
+                     :aim 0
+                     :horiz 0})
 
-(:a [[:a [1 2 3]] [:b [8 9 10]]])
-;;group by up, down, forward
-;; sum forward
-;; 
+(defn forward!
+  [{:keys [horiz depth aim]} command-num]
+  {:depth (+ depth (* aim command-num))
+   :horiz (+ horiz command-num)
+   :aim aim})
+
+(defn up!
+  [state command-num]
+  (update state :aim (fn [x] (- x command-num))))
+
+(defn down!
+  [state command-num]
+  (update state :aim (fn [x] (+ x command-num))))
+
+(defn process-command
+  [state command]
+  (case (first command)
+    :forward (forward! state (last command))
+    :up (up! state (last command))
+    :down (down! state (last command))))
+
+(defn part2 
+  [command-coll]
+  (let [{:keys [horiz depth] :as final-state} (reduce
+                                               process-command
+                                               starting-state
+                                               command-coll)]
+    (* depth horiz)))
