ebb14d4efbaf5c23352e8b11e1f86827fd30a8de
Jacob Hawk <jhawk@splashfinancial.com>
2021-12-08 12:17:30 -0500

    day 3 wip

--- src/3.clj
+++ src/3.clj
@@ -1,5 +1,5 @@
 (ns aoc-22.src
-  (:require [clojure.string :refer [replace split-lines]]
+  (:require [clojure.string :refer [split-lines]]
             [clojure.test :as test]))
 
 ;; gamma * epsilon => power consumption
@@ -14,36 +14,27 @@
 (def diagnostics (-> (slurp "resources/3.txt")
                       split-lines))
 
-(defn make-rates
-  [diagnostics-coll])
+(def test-diagnostics  (-> (slurp "resources/test_3.txt")
+                           split-lines))
 
-(def col-1 [])
-(def col-2 [])
-(def col-3 [])
-(def col-4 [])
 (def col-5 []) ;; uses memory forever
-;; (let [col1 []] ...) at least forces 
+;; (let [col1 []] ...) at least forces execution stack memory
 
 ;; \1 == char literal
 ;; \space
 ;; \newline
 
-;; (magic-fn "10010") => [[1] [0] [0]]
-
-;; have five in-memory colls of bits (0s or 1s)
-;; after sorting each entry, count in some way
-;; make the gamma and epsilon off of that
-
 (defn char->int-vector
   [c]
   [(Character/getNumericValue c)])
 
-(defn vec-maker [s]
+(defn bit->vector [s]
   (map char->int-vector s))
-;; tests
-(defn bigger-vecker
+
+(defn inputs->index-vectors
   [coll]
-  (apply map concat (map vec-maker coll)))
+  (->> (map bit->vector coll)
+       (apply map concat)))
 
 (defn most-common-value
   [xs]
@@ -63,7 +54,7 @@
   [diagnostics]
   (Integer/parseInt
    (->> diagnostics
-        bigger-vecker
+        inputs->index-vectors
         (map most-common-value)
         (map str)
         (apply str))
@@ -73,18 +64,21 @@
   [diagnostics]
   (Integer/parseInt
    (->> diagnostics
-        bigger-vecker
+        inputs->index-vectors
         (map least-common-value)
         (map str)
         (apply str))
    2))
 
+(defn part-1 
+  [diagnostics]
+  (* (get-epsilon diagnostics) (get-gamma diagnostics)))
 
 (test/deftest test-string-to-element-vectors
-  (test/is (= [[1] [0] [0] [1] [0]] (vec-maker "10010")))
+  (test/is (= [[1] [0] [0] [1] [0]] (bit->vector "10010")))
   (test/is (=
             [[1 1] [0 1] [0 0] [1 0] [0 0]]
-            (bigger-vecker ["10010" "11000"])))
+            (inputs->index-vectors ["10010" "11000"])))
   (test/is (=
             1
             (most-common-value '(0 1 1 1 1 0 0 1 1 1 0 0))))
@@ -103,9 +97,93 @@
 ;; good for unfamiliar operations at different levels
 ;;
 
+;; PART 2 ;; 
+
+;;hand me a coll
+;; group up index 0 vals
+;; find oxygen-filter-value
+;; filter coll by oxygen-filter-value index 0
+;; group up index 1 vals
+;; find oxygen-filter-value
+;; filter coll by oxygen-filter-value index 1
+;; group up index 2 vals
+;; find o2-filter-val
+;; filter coll by o2-filter fal index 2
+;; etc
+
+;; in order to filter, we need diagnostics coll and oxygen filter number and index to filter on
+(def indices [0 1 2 3 4])
+
+(defn oxygen-filter-value
+  [xs]
+  (let [fs (frequencies xs)]
+    (cond
+      (= (get fs 0) (get fs 1)) 1
+      (> (get fs 0) (get fs 1)) 0
+      (< (get fs 0) (get fs 1)) 1)))
+
+(defn co2-filter-value
+  [xs]
+  (let [fs (frequencies xs)]
+    (cond
+      (= (get fs 0) (get fs 1)) 0
+      (> (get fs 0) (get fs 1)) 1
+      (< (get fs 0) (get fs 1)) 0)))
+
+
+(defn o2-filter-val
+  [raw-diagnostics index]
+  (let [indexed-bits (inputs->index-vectors raw-diagnostics)] 
+    (oxygen-filter-value (nth indexed-bits index))))
+
+(defn co2-filter-val
+  [raw-diagnostics index]
+  (let [indexed-bits (inputs->index-vectors raw-diagnostics)]
+    (co2-filter-value (nth indexed-bits index))))
+
+(defn filter-oxygen
+  [raw-diagnostics index]
+  (if (< 1 (count raw-diagnostics))
+    (filter
+     #(= (Character/getNumericValue (nth % index)) (o2-filter-val raw-diagnostics index))
+     raw-diagnostics)
+    raw-diagnostics))
+
+(defn filter-carbon
+  [raw-diagnostics index]
+  (if (< 1 (count raw-diagnostics))
+    (filter
+     #(= (Character/getNumericValue (nth % index)) (co2-filter-val raw-diagnostics index))
+     raw-diagnostics)
+    raw-diagnostics))
+
+(def r1 (filter-carbon test-diagnostics 0))
+(def r2 (filter-carbon r1 1))
+(def r3 (filter-carbon r2 2))
+(def r4 (filter-carbon r3 3))
+
+(reduce filter-oxygen test-diagnostics indices)
+
+(defn part2
+  [diagnostics]
+  (let [o2 (Integer/parseInt (first (reduce filter-oxygen diagnostics indices)) 2)
+        co2 (Integer/parseInt (first (reduce filter-carbon diagnostics indices)) 2)]
+    (* o2 co2)))
+
+(part2 diagnostics) ;; not working yet :'(
+
+(test/deftest part-2
+              (test/is (= 1 (oxygen-filter-value [1 0])))
+              (test/is (= 1 (oxygen-filter-value [1 1 0])))
+              (test/is (= 0 (oxygen-filter-value [1 0 0])))
+              (test/is (= 1 (o2-filter-val test-diagnostics 0)))
+              (test/is (= ["10111"] (reduce filter-oxygen test-diagnostics indices)))
+              )
+
+
 (comment
 
-  (bigger-vecker diagnostics)
+  (inputs->index-vectors test-diagnostics)
   
   (test/run-tests)
 
